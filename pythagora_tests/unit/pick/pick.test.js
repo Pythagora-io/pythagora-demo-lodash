044b0676ef9fc2de7d7497982f8e960cf214853b
Robert Kieffer <robert@broofa.com>
2023-06-14 05:32:16 -0700

    correct pick() args

--- pythagora_tests/unit/pick/pick.test.js
+++ pythagora_tests/unit/pick/pick.test.js
@@ -1,8 +1,8 @@
 import pick from '../../../pick.js';
 
 describe('pick', () => {
-  test('pick({"a": 1, "b": "2", "c": 3}, ["a", "c"])', () => {
-    expect(pick({"a": 1, "b": "2", "c": 3}, ["a", "c"])).toEqual({"a": 1, "c": 3});
+  test('pick({"a": 1, "b": "2", "c": 3}, "a", "c")', () => {
+    expect(pick({"a": 1, "b": "2", "c": 3}, "a", "c")).toEqual({"a": 1, "c": 3});
   });
 
   test('pick(null, ["a", "c"])', () => {
@@ -29,15 +29,15 @@ describe('pick', () => {
     expect(pick({"a": 1, "b": "2", "c": 3}, ["b"])).toEqual({"b": "2"});
   });
 
-  test('pick({"a": 1, "b": {"c": 2, "d": 3}}, ["b.c"])', () => {
-    expect(pick({"a": 1, "b": {"c": 2, "d": 3}}, ["b.c"])).toEqual({"b": {"c": 2}});
+  test('pick({"a": 1, "b": {"c": 2, "d": 3}}, "b.c")', () => {
+    expect(pick({"a": 1, "b": {"c": 2, "d": 3}}, "b.c")).toEqual({"b": {"c": 2}});
   });
 
-  test('pick({"a": 1, "b": {"c": 2, "d": 3}}, ["b"])', () => {
-    expect(pick({"a": 1, "b": {"c": 2, "d": 3}}, ["b"])).toEqual({"b": {"c": 2, "d": 3}});
+  test('pick({"a": 1, "b": {"c": 2, "d": 3}}, "b")', () => {
+    expect(pick({"a": 1, "b": {"c": 2, "d": 3}}, "b")).toEqual({"b": {"c": 2, "d": 3}});
   });
 
-  test('pick({"a": 1, "b": "2", "c": 3}, ["a", "c", "d"])', () => {
-    expect(pick({"a": 1, "b": "2", "c": 3}, ["a", "c", "d"])).toEqual({"a": 1, "c": 3});
+  test('pick({"a": 1, "b": "2", "c": 3}, "a", "c", "d")', () => {
+    expect(pick({"a": 1, "b": "2", "c": 3}, "a", "c", "d")).toEqual({"a": 1, "c": 3});
   });
 });
